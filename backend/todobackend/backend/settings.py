387179e22c7ceb58b2aacbd22e4ad1b9ecf552fe
rahmat530 <rahmatullah.wahdat046@gmai.com>
2026-04-17 00:08:49 +0200

    fix: prepare django for production

--- backend/todobackend/backend/settings.py
+++ backend/todobackend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b^e6vb&)xm$p(h1uk(&2@34gffp%3tsw2_#ay$5%3f3=9rvjv('
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-b^e6vb&)xm$p(h1uk(&2@34gffp%3tsw2_#ay$5%3f3=9rvjv(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 REST_FRAMEWORK= {
   'DEFAULT_AUTHENTICATION_CLASSES':[
@@ -129,5 +133,7 @@ REST_FRAMEWORK= {
 }
 
 CORS_ORIGIN_WHITELIST = [
-'http://localhost:3000',
+    'http://localhost:3000',
+    'https://todoapp-fullstack.vercel.app',  # ← بعداً آدرس واقعی Vercel رو اینجا میذاری
 ]
+
