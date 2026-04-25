f46e4346444794b4f9a380c2e15595de5eaa19d5
rahmat530 <rahmatullah.wahdat046@gmai.com>
2026-04-26 01:34:59 +0200

    fix: hooks are updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
   // const user = null;
   const [user, setUser] = React.useState(null);
   const [token, setToken] = React.useState(null);
-  const [, setError] = useState(null);
+  const [error, setError] = React.useState("");
 
   async function login(user = null) {
     TodoDataService.login(user)
